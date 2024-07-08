db54d10506de5ea6f5ba300e3eb6f8c173a7f481
Hoa VM <hoavm.dev@gmail.com>
2024-07-08 21:51:43 +0700

    x

--- src/helpers/fetchAuthApi.js
+++ src/helpers/fetchAuthApi.js
@@ -8,18 +8,12 @@ import axios from 'axios';
 export default async function fetchAuthApi({ url, body }) {
   try {
     const accessToken = localStorage.getItem('ACCESS_TOKEN');
-    console.log({
-      method: 'GET',
-      url: `${process.env.REACT_APP_BASE_API_URL}${url}`,
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-      data: body,
-    });
+
     const resp = await axios({
       method: 'GET',
       url: `${process.env.REACT_APP_BASE_API_URL}${url}`,
       headers: {
+        'Access-Control-Allow-Origin': '*',
         Authorization: `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
       },
